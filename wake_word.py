e0fd3e0bc11cd4ebe543ac0e3d41f5ffb9abb414
Snalotaibi <samaaalotaibi02@gmail.com>
2025-04-25 00:55:55 -0700

    Create wake_word.py

---
+++ wake_word.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+def listen_for_wake_word(wake_word="hey aztec"):
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print(f"Waiting for '{wake_word}'...")
+        while True:
+            try:
+                audio = recognizer.listen(source)
+                phrase = recognizer.recognize_google(audio).lower()
+                if wake_word in phrase:
+                    print("Wake word detected!")
+                    return True
+            except:
+                continue
